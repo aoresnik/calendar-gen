da89a28729dc44e3f42149e8d21d464312082dfd
Andrej Orešnik <aoresnik7@gmail.com>
2023-12-31 22:22:39 +0100

    Format PDF with 2cm margin
    Usable for hand written notes

--- functions/__init__.py
+++ functions/__init__.py
@@ -5,6 +5,13 @@ import sys
 def generate_pdf_file(file_name_html, file_name_pdf, orientation="Landscape"):
   print("Generating PDF " + file_name_pdf + " with wkhtmltopdf", file=sys.stderr) 
   try:
-    subprocess.run(["wkhtmltopdf", "-O", orientation, file_name_html, file_name_pdf])
+    subprocess.run(["wkhtmltopdf", 
+                    "-O", orientation, 
+                    "--margin-top", "2cm",
+                    "--margin-left", "2cm",
+                    "--margin-right", "2cm",
+                    "--margin-bottom", "2cm",
+                    file_name_html, 
+                    file_name_pdf])
   except FileNotFoundError:
     print("ERROR: Could not find wkhtmltopdf - not generating PDF output. Plese install it to generate PDF output.", file=sys.stderr)
